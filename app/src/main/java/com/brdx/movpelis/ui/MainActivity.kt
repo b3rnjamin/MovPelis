fd3c31c9c5d22cfacbf16a123a0ec00e263555e0
benjamin <kvillanooliv@gmail.com>
2022-10-16 17:10:15 -0500

    update view list in MainActivity.kt

--- app/src/main/java/com/brdx/movpelis/ui/MainActivity.kt
+++ app/src/main/java/com/brdx/movpelis/ui/MainActivity.kt
@@ -4,17 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.brdx.movpelis.core.Resource
 import com.brdx.movpelis.core.RetrofitClient
 import com.brdx.movpelis.data.model.Movie
 import com.brdx.movpelis.databinding.ActivityMainBinding
-import com.brdx.movpelis.domain.login.LoginRepoImpl
 import com.brdx.movpelis.domain.main.MovieRepoImpl
-import com.brdx.movpelis.presentation.login.LoginViewModel
-import com.brdx.movpelis.presentation.login.LoginViewModelFactory
 import com.brdx.movpelis.presentation.main.MovieViewModel
 import com.brdx.movpelis.presentation.main.MovieViewModelFactory
 import com.brdx.movpelis.ui.adapter.MovieAdapter
@@ -42,7 +37,7 @@ class MainActivity : AppCompatActivity(), MovieAdapter.OnMovieListener {
         setContentView(binding.root)
 
         binding.rvMovies.apply {
-            layoutManager = GridLayoutManager(context, 4)
+            layoutManager = GridLayoutManager(context, 2)
             setHasFixedSize(true)
             adapter = movieAdapter
         }
@@ -63,7 +58,11 @@ class MainActivity : AppCompatActivity(), MovieAdapter.OnMovieListener {
                 }
                 is Resource.Failure -> {
                     val exception = resource.exception
-                    Snackbar.make(binding.root, (exception.message ?: "Error unexpected").toString(), Snackbar.LENGTH_SHORT)
+                    Snackbar.make(
+                        binding.root,
+                        (exception.message ?: "Error unexpected").toString(),
+                        Snackbar.LENGTH_SHORT
+                    )
                         .show()
                 }
             }
