65aca30b6ab0ab781689cb582a35e7964a0f5bf8
benjamin <kvillanooliv@gmail.com>
2022-10-16 21:23:28 -0500

    fix movie parcelable

--- app/src/main/java/com/brdx/movpelis/data/local/MovieDao.kt
+++ app/src/main/java/com/brdx/movpelis/data/local/MovieDao.kt
@@ -11,6 +11,6 @@ interface MovieDao {
     @Query("SELECT * FROM movie_entity")
     suspend fun getListMovies(): List<MovieEntity>
 
-    @Insert(onConflict = OnConflictStrategy.ABORT)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveMovie(movie: MovieEntity)
 }
--- app/src/main/java/com/brdx/movpelis/data/model/Movie.kt
+++ app/src/main/java/com/brdx/movpelis/data/model/Movie.kt
@@ -1,5 +1,9 @@
 package com.brdx.movpelis.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Movie(
     val id: String = "",
     val poster_path: String = "",
@@ -7,4 +11,4 @@ data class Movie(
     val vote_average: Double = 0.0,
     val release_date: String = "",
     val overview: String = ""
-)
+) : Parcelable
